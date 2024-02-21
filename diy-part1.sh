b154fa2ce148af58c847e03113c1fce2543117c4
Drliao2023 <128355309+Drliao2023@users.noreply.github.com>
2024-02-21 22:53:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
+git clone https://github.com/ximiTech/luci-app-msd_lite package/luci-app-msd_lite
